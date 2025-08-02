6d88e4692fd2fc73965de9de7d728b550b27e2aa
arrassiibtissame <ibtissame2.ar@gmail.com>
2025-08-02 14:38:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-/**
- * @format
- */
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './package.json';
+const resources = {
+  fr: {
+    translation: {
+      welcome: "Bienvenue sur LaitGO",
+      email: "Email",
+      password: "Mot de passe",
+      login: "Se connecter"
+    }
+  },
+  ar: {
+    translation: {
+      welcome: "مرحبا بك في LaitGO",
+      email: "البريد الإلكتروني",
+      password: "كلمة المرور",
+      login: "تسجيل الدخول"
+    }
+  },i
+  // Ajoute ici Darija si tu veux
+};
 
-AppRegistry.registerComponent(appName, () => App); 
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'fr', // Change la langue ici
+    fallbackLng: 'fr',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n; 
